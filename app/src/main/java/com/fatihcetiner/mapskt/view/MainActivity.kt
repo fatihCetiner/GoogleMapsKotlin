ba3fc24fdd638f26c37aec9fda878c51ded39444
Fatih Furkan Çetiner <f-cetiner99@hotmail.com>
2022-06-14 13:34:49 +0300

    Google Maps Project

---
+++ app/src/main/java/com/fatihcetiner/mapskt/view/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.fatihcetiner.mapskt.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import com.fatihcetiner.mapskt.R
+import com.fatihcetiner.mapskt.adapter.PlaceAdapter
+import com.fatihcetiner.mapskt.databinding.ActivityMainBinding
+import com.fatihcetiner.mapskt.model.Place
+import com.fatihcetiner.mapskt.roomdb.PlaceDao
+import com.fatihcetiner.mapskt.roomdb.PlaceDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db : PlaceDatabase
+    private lateinit var placeDao: PlaceDao
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        db = Room.databaseBuilder(applicationContext,PlaceDatabase::class.java,"Places").build()
+        placeDao = db.placeDao()
+
+        compositeDisposable.add(
+            placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResponse)
+        )
+
+    }
+
+    private fun handleResponse(placeList : List<Place>){
+        binding.recyclerView.layoutManager =LinearLayoutManager(this)
+        val adapter = PlaceAdapter(placeList)
+        binding.recyclerView.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_place){
+            val intent = Intent(this, MapsActivity::class.java)
+            intent.putExtra("info","new")
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
